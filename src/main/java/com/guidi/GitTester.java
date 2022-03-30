4171fa4d351737d20603ae092e099ccd0b2bd357
HuRongBo <489269579@qq.com>
2022-03-30 20:09:11 +0800

    hot-fix first commit

--- src/main/java/com/guidi/GitTester.java
+++ src/main/java/com/guidi/GitTester.java
@@ -8,5 +8,6 @@ public class GitTester {
     public static void main(String[] args) {
         System.out.println("hello git2");
         System.out.println("hello git3");
+        System.out.println("hello fix");
     }
 }
