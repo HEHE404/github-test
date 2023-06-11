493f533a0aa004ddc9ce5412e7977124018b14ce
HuRongBo <489269579@qq.com>
2023-06-11 21:50:19 +0800

    hello world!

---
+++ src/main/java/com/guidi/HelloApplication.java
@@ -0,0 +1,15 @@
+package com.guidi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author haha
+ * @create 2023-06-11 21:47
+ */
+@SpringBootApplication
+public class HelloApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(HelloApplication.class,args);
+    }
+}
---
+++ src/main/java/com/guidi/controller/HelloController.java
@@ -0,0 +1,16 @@
+package com.guidi.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author haha
+ * @create 2023-06-11 21:46
+ */
+@RestController
+public class HelloController {
+    @GetMapping("/hello")
+    public String sayHello(){
+        return "hello world!";
+    }
+}
