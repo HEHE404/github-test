cf076bd43d9cf4badfef9b0ea9490c0a30eccb73
HuRongBo <489269579@qq.com>
2023-06-14 23:02:57 +0800

    r first commit

---
+++ src/main/java/com/guidi/RealeaseTest.java
@@ -0,0 +1,11 @@
+package com.guidi;
+
+/**
+ * @author haha
+ * @create 2023-06-14 23:02
+ */
+public class RealeaseTest {
+    public static void main(String[] args) {
+        System.out.println("r first commit");
+    }
+}
