4ceede0ae5c3ab177ab181bc63bdf359e2df3d27
HuRongBo <489269579@qq.com>
2023-06-14 23:00:57 +0800

    a m 4

--- src/main/java/com/guidi/GitTester.java
+++ src/main/java/com/guidi/GitTester.java
@@ -16,5 +16,6 @@ public class GitTester {
         System.out.println("master add 2");
         System.out.println("f add 1");
         System.out.println("m add 3");
+        System.out.println("m add 4");
     }
 }
